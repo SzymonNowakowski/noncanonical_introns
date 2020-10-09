c7167df1a9a2c755ba81de7f87bb12158cf991af
jrozycka <j.rozycka@hotmail.com>
2020-10-09 14:14:57 +0200

    jupyter workflow added

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,10 @@
-from Bio.SeqIO.FastaIO import SimpleFastaParser
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
 
 
 def cut_junctions(cut, sequences):
-    '''0 - dont cut, 1 - only conventional, 2 - both'''
+    '''Preprocess data in different ways by cutting off the conventional or all splices.
+    0 - dont cut, 1 - only conventional, 2 - both'''
     new_seqs = []
     for i in sequences:
         if cut == 0:
@@ -32,43 +31,10 @@ def split(k, seqs):
 
 
 def preprocess(cut, k, sequences, file_out):
+    """Optionally cut off junctions, then split into k-length oligonucleotides and convert into TF_IDF."""
     sequences_cut = cut_junctions(cut, [x[1] for x in sequences])
     split_seqs = split(k, sequences_cut)
     tfidf_vectorizer = TfidfVectorizer(use_idf=True, lowercase=False)
     tfidf_vectorizer_vectors = tfidf_vectorizer.fit_transform(split_seqs)
     print(type(tfidf_vectorizer_vectors))
     np.save(file_out, tfidf_vectorizer_vectors)
-
-#
-# def kmeans():
-#
-
-def main():
-    file = '/home/rozycka/sequences_euglena/good_introns50+3.fasta'
-    # file = '/home/julia/Documents/licencjat/good_introns50+3.fasta'
-    with open(file, "r") as handle:
-        sequences = list(SimpleFastaParser(handle))[:100]
-
-    types = []
-    for s in sequences:
-        left_anchor = s[1][3:5]
-        right_anchor = s[1][-5:-3]
-        if left_anchor == 'GT' and right_anchor == 'AG' or left_anchor == 'CT' and right_anchor == 'AC':
-            types.append(0)
-        else:
-            types.append(1)
-
-    preprocess(0, 4, sequences, './seq_4_no_cut.npy')
-    seq4 = np.load('./seq_4_no_cut.npy')
-    for x in [2**x for x in range(2,3)]:
-        kmeans = KMeans(n_clusters=x, init='k-means++', max_iter=50, n_init=1, random_state=0, n_jobs=8)
-        kmeans.fit(seq4)
-        print(kmeans.labels_)
-        print(type(kmeans.labels_))
-
-
-    # preprocess(0, 7, sequences, './seq_7_no_cut.npy')
-
-
-if __name__ == "__main__":
-    main()
