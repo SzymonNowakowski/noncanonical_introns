9e2ee1adfe521eb00a917c437e0375a9fa20947b
halwap <pawelhalakuc@gmail.com>
2021-01-07 14:28:07 +0100

    Added expanded_sequence (+/-500nt) to the Gene class.

--- introns.py
+++ introns.py
@@ -274,7 +274,7 @@ class Gene(GenomicSequence):
         expansion_right = end - self.end
         return expanded_sequence, expansion_left, expansion_right
 
-    def get_transcript_sequence(self, expanded = False):
+    def get_transcript_sequence(self):
         exons_seqs = []
         for exon in self.exons:
             exons_seqs.append((exon.sequence, exon.start))
@@ -283,11 +283,9 @@ class Gene(GenomicSequence):
         elif self.strand == '-':
             exons_seqs.sort(key=lambda tup: tup[1], reverse=True)
         sequence = ''.join([exon_seq[0] for exon_seq in exons_seqs])
-        if expanded:
-            sequence = self.expansion_left * '-' + sequence + self.expansion_right * '-'
         return sequence
 
-    def get_transcript_with_gaps_sequence(self):
+    def get_transcript_with_gaps_sequence(self, expanded = False):
         to_be_joined = []
         start, end = None, None
         reverse = True if self.strand == '-' else False
@@ -307,6 +305,8 @@ class Gene(GenomicSequence):
                 to_be_joined.append(exon.sequence)
                 end = exon.start
         sequence = ''.join(to_be_joined)
+        if expanded:
+            sequence = self.expansion_left * '-' + sequence + self.expansion_right * '-'
         return sequence
 
     def create_introns(self):
